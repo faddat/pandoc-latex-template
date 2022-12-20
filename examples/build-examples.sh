131736d6e2fd493a8642ec889376cd8da608739f
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-20 22:04:56 +0700

    Update build-examples.sh

--- examples/build-examples.sh
+++ examples/build-examples.sh
@@ -6,26 +6,10 @@ echo "# building examples"
 echo "##"
 echo ""
 
-containsElement () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
-# these examples are skipped because they don't run on travis (for now)
-skippedExamples=("language-chinese" "language-japanese")
 
 # loop all files in the current folder
 for f in *; do
-	# run only for folders
-    if [ -d "$f" ]; then
-
-    	if containsElement "$f" "${skippedExamples[@]}"; then
-    		echo "skipping '$f'"
-    		echo ""
-    	else
-    		echo "building '$f'"
+		echo "building '$f'"
     		cd "$f"
     		echo "    - running pandoc build script"
         	bash "$PWD/build.sh"
@@ -33,6 +17,6 @@ for f in *; do
         	pdftoppm -r 150 -png "document.pdf" > "preview.png"
         	echo ""
         	cd ".."
-    	fi
-    fi
+    	
+    
 done
